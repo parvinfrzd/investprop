41f27b0fba2fe3cf814d55ee5624f8447ac3dc42
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-23 18:51:42 -0400

    add user api route to server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // require all routes here: 
-
+app.use('/api/users', require('./routes/api/users'));
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
 app.get('/*', function(req, res) {
