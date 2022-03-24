cd59090716eea8a5373613e71595ae54ebb370fb
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-23 20:42:37 -0400

    complete create model in controller

--- controllers/investments.js
+++ controllers/investments.js
@@ -1,12 +1,17 @@
 const InvestmentModel = require('../models/Investment');
 
 async function create(req, res){ 
-    console.log(req.body);
-    // try {
-        
-    // } catch(e) {
-
-    // }
+    console.log('request body is',req.body);
+    try {
+        await InvestmentModel.create({
+            name: req.body.name, 
+            description: req.body.description, 
+            value: parseInt(req.body.value)
+        });
+        res.status(200).json('ok')
+    } catch(e) {
+        res.status(400).json(e);
+    }
 }
 
 module.exports = {
