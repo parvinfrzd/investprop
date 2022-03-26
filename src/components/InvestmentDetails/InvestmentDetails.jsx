dc7180c59db57282e9ebb3e8be603203c02ed296
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-25 21:29:39 -0400

    investment details refactor

--- src/components/InvestmentDetails/InvestmentDetails.jsx
+++ src/components/InvestmentDetails/InvestmentDetails.jsx
@@ -11,7 +11,7 @@ export default function InvestmentDetails(props) {
             <div className="align-rt">
                 <div>Property name: <span className="smaller">{props.investment.name}</span></div>
                 <div>Description: <span className="smaller">{props.investment.description}</span></div>
-                <div>Value: <span className="smaller">{props.investment.value}</span></div>
+                <div>Value: <span className="smaller">${props.investment.value}</span></div>
             </div>
         </div>
     )
