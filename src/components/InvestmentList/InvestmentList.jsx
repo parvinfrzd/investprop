b02baaca6ede50309f3f618bdf02e675886d0cfb
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-25 21:30:34 -0400

    investment list update

--- src/components/InvestmentList/InvestmentList.jsx
+++ src/components/InvestmentList/InvestmentList.jsx
@@ -2,6 +2,7 @@ import './InvestmentList.css';
 import InvestmentDetails from '../InvestmentDetails/InvestmentDetails';
 
 export default function InvestmentList(props){
-    
+    let investmentList = props.investment.map
 }
 
+// . property num 1   des: fkgjnj value: 1234
