11c5a34a8650535b3a6185f163d5176d7ab7d9d7
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-25 21:29:15 -0400

    add get routes for investment

--- routes/api/investments.js
+++ routes/api/investments.js
@@ -4,5 +4,5 @@ const investmentCtrl = require('../../controllers/investments');
 
 // Route handler for POSTing a new order. Full address will be POST /api/orders
 router.post('/', investmentCtrl.create)
-
+router.get('/',investmentCtrl.index)
 module.exports = router;
