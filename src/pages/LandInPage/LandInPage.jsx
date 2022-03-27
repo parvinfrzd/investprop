5bdd0d1f69e4c89c0305799139f31285cd0a36d4
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-27 16:35:29 -0400

    add twitter search component to landin page

--- src/pages/LandInPage/LandInPage.jsx
+++ src/pages/LandInPage/LandInPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import UserLogOut from '../../components/UserLogOut/UserLogOut';
 import InvestForm from "../../components/forms/InvestForm/InvestForm";
-
+import TwitterSearch from "../../components/TwitterSearch/TwitterSearch";
 class LandInPage extends React.Component {
 
   // initial state of the app when it first loads
@@ -21,6 +21,7 @@ class LandInPage extends React.Component {
           <UserLogOut />
         </nav>
         <InvestForm/>
+        <TwitterSearch/>
       </main>
     );
   }
