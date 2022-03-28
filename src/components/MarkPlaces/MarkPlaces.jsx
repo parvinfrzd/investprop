537d077fc090d7eccce8ad0cb1ff5060fc857b50
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-28 18:15:43 -0400

    load google maps

--- src/components/MarkPlaces/MarkPlaces.jsx
+++ src/components/MarkPlaces/MarkPlaces.jsx
@@ -1,17 +1,81 @@
 import React from 'react'; 
-import {GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api'; 
+import {GoogleMap, useJsApiLoader, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api'; 
 import {formatRelative} from 'date-fns';
-
+import mapStyles from './mapStyles';
 const libraries = ['Places']
-
-export default function MarkPlaces() {
-    const {isLoaded, loadError} = useLoadScript({
-        googleMapsApiKey : process.env.GOOGLE_MAPS_API_KEY, 
-        libraries
-    });
-
-    if(loadError) return 'Error loading maps';
-    if(!isLoaded) return 'Loading map in progress';
-
-    return <div>MAP</div>
+const mapContainerStyle = {
+    width: '100vw', 
+    height: '100vh'
 }
+const center = {
+    lat: 43.653225,
+    lng: -79.383186
+}
+const options = {
+    styles: mapStyles, 
+    disableDefaultUI: true, 
+    zoomControl: true
+}
+export default function MarkPlaces() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: 'apikey'
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        setMap(map)
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={10}
+        options={options}
+        onClick={(event) => {console.log(event)}}
+        >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+        </GoogleMap>
+    ) : <></>
+}
+
+
+
+// const { isLoaded } = useJsApiLoader({
+//     id: 'google-map-script',
+//     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+//   })
+
+//   const [map, setMap] = React.useState(null)
+
+//   const onLoad = React.useCallback(function callback(map) {
+//     const bounds = new window.google.maps.LatLngBounds();
+//     map.fitBounds(bounds);
+//     setMap(map)
+//   }, [])
+
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
+
+//   return isLoaded ? (
+//       <GoogleMap
+//         mapContainerStyle={mapContainerStyle}
+//         center={center}
+//         zoom={10}
+//         onLoad={onLoad}
+//         onUnmount={onUnmount}
+//       >
+//         { /* Child components, such as markers, info windows, etc. */ }
+//         <></>
+//       </GoogleMap>
+//   ) : <></>
