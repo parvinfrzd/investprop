4206efa765a4822b4be4388b75432af1ffea5efa
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-29 19:53:47 -0400

    cleanup

--- src/components/forms/InvestForm/InvestForm.jsx
+++ src/components/forms/InvestForm/InvestForm.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import InvestmentList from '../../InvestmentList/InvestmentList';
 export default class InvestForm extends Component {
     state = {
         name: '',
         description: '',
         value: '',
+        address: '',
         error: '', 
         investment : []
     };
@@ -14,17 +16,6 @@ export default class InvestForm extends Component {
             error: ''
             });
     }
-
-    fetchInvestments = async () => {
-        try {
-            const jsonInvestments = await fetch('/api/investments')
-            let investmentRes = await jsonInvestments.json()
-            if (!jsonInvestments.ok) throw new Error("Couldn't fetch investments")
-            this.setState({ investment: investmentRes});
-        } catch (error) {
-            console.error('ERROR: ', error);
-        }
-    }
   
     handleSubmit = async (evt) => {
         evt.preventDefault();
@@ -32,56 +23,56 @@ export default class InvestForm extends Component {
             const reqBody = {
                 name: this.state.name, 
                 description: this.state.description, 
+                address: this.state.address,
                 value: this.state.value,
             }
             let fetchResponse = await fetch("/api/investments",{
                 method: "POST", 
                 headers: {"Content-Type": "application/json"},
-                body : JSON.stringify({name: this.state.name, description: this.state.description, value:this.state.value})
+                body : JSON.stringify(reqBody)
             })
             let serverResponse = await fetchResponse.json()
             console.log("Success submitting investment:", serverResponse); 
             this.setState({
                 name: '',
                 description: '',
+                address: '',
                 value: '',
             })
-            this.fetchInvestments();
+            InvestmentList.fetchInvestments();
         }catch(e){
             console.log("Investment error", e)
             this.setState({ error: 'Submit failed' });
         }
     }
 
-    
-
-    async componentDidMount() {
-        this.fetchInvestments();
-    }
-
       
   
     render() {
       return (
-        <div className="form-container">
-              <form onSubmit={this.handleSubmit}>
-                  <label>Name:</label>
-                  <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
-                  <label>Description:</label>
-                  <input type="text" name="description" value={this.state.description} onChange={this.handleChange} />
-                  <label>Value:</label>
-                  <input type="text" name="value" pattern="[0-9]*" onInput={this.handleChange} value={this.state.value} />
-
-                  <button type="submit">Submit</button>
-              </form>
-              <div>
-                  {this.state.investment.length > 0 ? 
-                //   <p>We have investments!!</p>
-                    this.state.investment.map(inv =>  <li>{inv.name}-----{inv.description}-----{inv.value}</li>)
-                  :
-                  <p>Add some investments!!</p>
-                  }
-              </div>
+        <div className="card">
+            <div class="card-body">
+                <h5 class="card-title">Add to your investment wishlist</h5>
+                <form className="form-outline mb-4" onSubmit={this.handleSubmit}>
+                    <div class="form-group">
+                        <label className="form-label" for="form5Example1">Name:</label>
+                        <input type="text" id="form5Example1" className="form-control" name="name" value={this.state.name} onChange={this.handleChange} />
+                    </div>
+                    <div class="form-group">
+                        <label className="form-label" for="form5Example1">Description:</label>
+                        <input type="text" id="form5Example1" className="form-control" name="description" value={this.state.description} onChange={this.handleChange} />
+                    </div>
+                    <div class="form-group">
+                        <label className="form-label" for="form5Example1">Address:</label>
+                        <input type="text" id="form5Example1" className="form-control" name="address" value={this.state.address} onChange={this.handleChange} />
+                    </div>
+                    <div class="form-group">
+                    <label className="form-label" for="form5Example1">Price:</label>
+                    <input type="text" id="form5Example1" className="form-control" name="value" pattern="[0-9]*" onInput={this.handleChange} value={this.state.value} />
+                    </div>
+                    <button className="btn btn-primary" type="submit">Add investment</button>
+                </form>
+            </div>
           </div>
 
 
