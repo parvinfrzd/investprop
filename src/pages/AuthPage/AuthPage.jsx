5076760c105f120bb1f5a8215791d9731c163e44
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-29 19:55:13 -0400

    update styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -8,23 +8,25 @@ import SignUpForm from '../../components/forms/SignUpForm/SignUpForm';
 
 export default class AuthPage extends React.Component {
   state = {
-    showLogin: false,
+    showLogin: true,
   }
 
   render() {
     return (
-      <main className="AuthPage">
-        <div>
-          <h3 onClick={() => this.setState({ showLogin: !this.state.showLogin })}>
-            {this.state.showLogin ? 'SIGN UP' : 'LOG IN'}
-          </h3>
-        </div>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        
         {/* Another ternary operator! */}
         {/* If showLogin is true, show the login form. If false, show the signup form */}
         {this.state.showLogin ? 
         <SigninForm setUserInState={this.props.setUserInState}/> : 
         <SignUpForm setUserInState={this.props.setUserInState} />}
-      </main>
+
+        <div className='p-2'>
+          <p onClick={() => this.setState({ showLogin: !this.state.showLogin })}>
+            {this.state.showLogin ? 'Not a member? Register' : 'Already have an account? Signin'}
+          </p>
+        </div>
+      </div>
     );
   }
 }
