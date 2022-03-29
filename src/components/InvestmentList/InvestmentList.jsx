30d4bee23cd5308c4ad038c4e1c76b0ba21fcc1c
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-29 19:55:56 -0400

    update list of investments

--- src/components/InvestmentList/InvestmentList.jsx
+++ src/components/InvestmentList/InvestmentList.jsx
@@ -1,8 +1,54 @@
 import './InvestmentList.css';
-import InvestmentDetails from '../InvestmentDetails/InvestmentDetails';
+import React, { Component } from 'react';
 
-export default function InvestmentList(props){
-    let investmentList = props.investment.map
+
+export default class InvestmentList extends Component {
+    state = {
+       investments: []
+    }
+
+    fetchInvestments = async () => {
+        try {
+            const jsonInvestments = await fetch('/api/investments')
+            let investmentRes = await jsonInvestments.json()
+            if (!jsonInvestments.ok) throw new Error("Couldn't fetch investments")
+            this.setState({ investments: investmentRes});
+        } catch (error) {
+            console.error('ERROR: ', error);
+        }
+    }
+
+    async componentDidMount() {
+        this.fetchInvestments();
+    }
+    render() {
+        return (
+            <div className="card list-card">
+                <br/>
+                <h1 class="list-card-title">List of investments:</h1>
+            <div class="scrolling-wrapper">
+                    {this.state.investments.length > 0 ? 
+                        this.state.investments.map(inv =>  
+                            <div className="card">
+                                <div class="card-body">
+                                    <h3 class="card-title">{inv.name}</h3>
+                                    <br/>
+                                    <h6 class="card-subtitle mb-2 text-muted">Info:</h6>
+                                    <p class="card-text">{inv.description}</p>
+                                    <h6 class="card-subtitle mb-2 text-muted">Address:</h6>
+                                    <p class="card-text">{inv.address}</p>
+                                    <h4 class="card-title">Price:</h4>
+                                    <h5 class="card-title">$${inv.value}</h5>
+
+                                </div>
+                            </div>)
+                    :
+                    <p>Add some investments!!</p>
+                    }
+          </div>
+          </div>
+        );
+      }
+
 }
 
-// . property num 1   des: fkgjnj value: 1234
