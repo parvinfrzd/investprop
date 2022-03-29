4cbf29c35bb919dc39f1a4cfb3a2b1a0f7751be8
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-29 19:53:35 -0400

    create logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import AuthPage from './pages/AuthPage/AuthPage'
 import LandInPage from './pages/LandInPage/LandInPage';
+import UserLogOut from './components/UserLogOut/UserLogOut';
+
 // import LandinPage from './components/LandinPage/LandinPage';
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends Component {
   state = {
@@ -12,20 +15,33 @@ class App extends Component {
 
   setUserInState = (incomingUserData) => {
     this.setState({ user: incomingUserData })
+
   }
   componentDidMount() {
     let token = localStorage.getItem('token')
     if (token) {
       // YOU DO: check expiry!
       let userDoc = JSON.parse(atob(token.split('.')[1])).user // decode jwt token
-      this.setState({user: userDoc})      
+      this.setState({user: userDoc}) 
+      console.log(this.user)  
+   
     }
+  }
+
+  logoutUser = () => {
+    this.setState({ user : null});
   }
 
   render() {
     return (
       this.state.user ?
-            <LandInPage />
+            <div>
+              <Navbar bg="primary" variant="dark">
+                <UserLogOut logoutUser={this.logoutUser}/>
+              </Navbar>
+              <br />
+              <LandInPage />
+            </div>
             :
             <AuthPage setUserInState={this.setUserInState} />
     );
