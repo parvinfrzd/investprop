145268f774bc8af8fad4adb4af51d166f84fd299
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-29 19:54:18 -0400

    add styling to signup form

--- src/components/forms/SignUpForm/SignUpForm.jsx
+++ src/components/forms/SignUpForm/SignUpForm.jsx
@@ -65,19 +65,25 @@ export default class SignUpForm extends Component {
     const disable = this.state.password !== this.state.confirm;
     return (
       <div>
-        <div className="form-container">
           <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-            <label>Email</label>
-            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-            <label>Password</label>
-            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-            <label>Confirm</label>
-            <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <button type="submit" disabled={disable}>SIGN UP</button>
+            <div class="form-group">
+              <label for="exampleInputName1">Name</label>
+              <input type="text" class="form-control" id="exampleInputName1" name="name" value={this.state.name} onChange={this.handleChange} required />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" name="email" value={this.state.email} onChange={this.handleChange} required />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={this.state.password} onChange={this.handleChange} required />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPasswor2">Confirm</label>
+              <input type="password" className="form-control" id="exampleInputPassword2" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+            </div>
+            <button type="submit" className="btn btn-primary" disabled={disable}>SIGN UP</button>
           </form>
-        </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
       </div>
     );
