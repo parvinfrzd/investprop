fb49d3426d7ab01ee5aef11992ec4423fd22aeea
Parvin Farahzadeh <parv93farah@gmail.com>
2022-04-01 18:41:28 -0400

    authpage styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -5,6 +5,7 @@ import './AuthPage.css';
 import SigninForm from '../../components/forms/SigninForm/SinginForm';
 import SignUpForm from '../../components/forms/SignUpForm/SignUpForm';
 
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default class AuthPage extends React.Component {
   state = {
@@ -13,19 +14,29 @@ export default class AuthPage extends React.Component {
 
   render() {
     return (
-      <div className="d-flex flex-column justify-content-center align-items-center">
-        
-        {/* Another ternary operator! */}
-        {/* If showLogin is true, show the login form. If false, show the signup form */}
-        {this.state.showLogin ? 
-        <SigninForm setUserInState={this.props.setUserInState}/> : 
-        <SignUpForm setUserInState={this.props.setUserInState} />}
+      <div>
+          <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">InvestProp</Navbar.Brand>
+          </Navbar>
+          <br/>
 
-        <div className='p-2'>
-          <p onClick={() => this.setState({ showLogin: !this.state.showLogin })}>
-            {this.state.showLogin ? 'Not a member? Register' : 'Already have an account? Signin'}
-          </p>
+      <h5 className='text-center'>Start using this app by loging in</h5>
+      <br/>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className='card'>
+        <div class="card-body">
+          {this.state.showLogin ? 
+          <SigninForm setUserInState={this.props.setUserInState}/> : 
+          <SignUpForm setUserInState={this.props.setUserInState} />}
+
+          <div className='p-2'>
+            <p onClick={() => this.setState({ showLogin: !this.state.showLogin })}>
+              {this.state.showLogin ? 'Not a member? Register' : 'Already have an account? Signin'}
+            </p>
+          </div>
         </div>
+        </div>
+      </div>
       </div>
     );
   }
