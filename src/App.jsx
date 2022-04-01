71fb9f3a4c9ad54d0dcd50c5eeb0db187ead79c9
Parvin Farahzadeh <parv93farah@gmail.com>
2022-04-01 19:03:28 -0400

    update routes and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+} from "react-router-dom";
 import './App.css';
 import AuthPage from './pages/AuthPage/AuthPage'
 import LandInPage from './pages/LandInPage/LandInPage';
 import UserLogOut from './components/UserLogOut/UserLogOut';
 
 // import LandinPage from './components/LandinPage/LandinPage';
-import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, Container  } from 'react-bootstrap';
+import TwitterSearch from './pages/TwitterSearch/TwitterSearch';
+import InvestmentDetails from './components/InvestmentDetails/InvestmentDetails';
 
 class App extends Component {
   state = {
     user: null,
+    username: '',
+    useremail: '',
   }
 
   setUserInState = (incomingUserData) => {
@@ -20,30 +29,52 @@ class App extends Component {
   componentDidMount() {
     let token = localStorage.getItem('token')
     if (token) {
-      // YOU DO: check expiry!
       let userDoc = JSON.parse(atob(token.split('.')[1])).user // decode jwt token
-      this.setState({user: userDoc}) 
+      this.setState({user: userDoc, username: userDoc.name, useremail: userDoc.email}) 
       console.log(this.user)  
    
     }
   }
 
-  logoutUser = () => {
-    this.setState({ user : null});
+  userLogOut = () => {
+    let token = localStorage.getItem('token');
+    token = null;
+    localStorage.removeItem('token');
+    this.setState({user: null})      
   }
 
   render() {
     return (
       this.state.user ?
             <div>
-              <Navbar bg="primary" variant="dark">
-                <UserLogOut logoutUser={this.logoutUser}/>
+              <Navbar className='container-fluid' expand="lg" bg="dark" variant="dark">
+              {/* <Container> */}
+              <Nav className="me-auto">
+                <Navbar.Brand href="#home">InvestProp</Navbar.Brand>
+                <Nav.Link href="/">My Dashboard</Nav.Link>
+                <Nav.Link href="/twits">Search News</Nav.Link>
+                <Nav.Link href="/invests">View all investments</Nav.Link>
+              </Nav>
+              <br></br><br></br>
+              <Nav>
+                <UserLogOut name={this.state.username} logoutUser={this.userLogOut}/>
+              </Nav>
+              {/* </Container> */}
+
               </Navbar>
-              <br />
-              <LandInPage />
+
+              <Routes>
+                  <Route path='/' element={<LandInPage />}/>
+                  <Route path='/twits'element={<TwitterSearch />}/>
+                  <Route path='/invests'element={<InvestmentDetails />}/>
+
+              </Routes>  
             </div>
             :
+            <div>
+            
             <AuthPage setUserInState={this.setUserInState} />
+            </div>
     );
   }
 }
